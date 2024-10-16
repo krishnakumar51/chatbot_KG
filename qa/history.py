5dca2606939c32e2e1df85e8220032cdbd7ae6f2
Krishna kumar <godkrishnasskal@gmail.com>
2024-10-16 17:41:51 +0530

    op

--- qa/history.py
+++ qa/history.py
@@ -233,9 +233,8 @@ def create_chain(llm, graph, vector_index, chat):
     - If the input is in 'Hindi or Roman Hindi' (e.g., "Bharat ki rajdhani kya hai?"), respond in Hindi.
     - If the input is in 'Punjabi or Roman Punjabi' (e.g., "Bharat di rajdhani ki hai?"), respond in Punjabi.
     - If the input is in English or any other language, respond in English.
-    If you don't know the answer, respond with 'मुझे नहीं पता।' for Hindi, 'ਮੈਂ ਨਹੀਂ ਜਾਣਦਾ।' for Punjabi, or 'I don't know.' for English.
+    If you don't know the answer, respond with 'Please reach out to our customer service on WhatsApp:[Contact Support](https://api.whatsapp.com/send/?phone=9105575000)'.
     Use the provided context and chat history to answer the question directly. 
-    If you don't know the answer, respond with 'Please reach out to our customer service on WhatsApp:[Contact Support](https://api.whatsapp.com/send/?phone=9105575000)'. 
     Limit your response to a few sentences. Avoid unnecessary details or lengthy explanations. 
     Do not reference the context or chat history explicitly and don't mention 'According to the provided context'.
 
